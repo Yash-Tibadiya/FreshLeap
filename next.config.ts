3695a190a452b64de4f41e3d9437454642dd203b
Yash-Tibadiya <tibadiyayash@gmail.com>
2025-04-01 23:19:11 +0530

    Update image configuration in next.config.ts to allow unoptimized images from all domains

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["localhost"], // Add any other domains you're using
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-        pathname: "/_next/image/**",
+        protocol: "https",
+        hostname: "**",
+        pathname: "**",
       },
     ],
+    unoptimized: true, // This allows images from all domains without optimization
   },
 };
 
